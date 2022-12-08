67d847082964978f8b5022d26de4331fb03c2dde
EosReign <vsevolod_sol@mail.ru>
2022-12-08 03:48:46 +0300

    meh

---
+++ src/main/java/prosky/sphw43rightver/repository/AvatarRepository.java
@@ -0,0 +1,14 @@
+package prosky.sphw43rightver.repository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+import prosky.sphw43rightver.entity.Avatar;
+
+import java.util.Optional;
+
+@Repository
+public interface AvatarRepository extends PagingAndSortingRepository<Avatar, Long> {
+    Optional<Avatar> findByStudentId(long studentId);
+
+    void save(Avatar avatar);
+}
